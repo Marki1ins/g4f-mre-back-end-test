4a0d279bcd87ece3815c4e32f78e3c37de5ab955
Marki1ins <marcosdfcarvalhoo@gmail.com>
2025-01-28 13:54:46 -0300

    feat: :wrench: add some adjusts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,51 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import helmet from "helmet";
+import { LoggerErrorInterceptor } from "nestjs-pino";
+
+import { AppModule } from "./app.module";
+import { BadRequestException, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: "*",
+      methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+    logger: ["error", "warn", "debug", "log", "verbose"],
+  });
+  app.use(helmet());
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.map((err) => {
+          if (err.constraints) {
+            return Object.values(err.constraints).join(", ");
+          }
+          return "";
+        });
+
+        return new BadRequestException(formattedErrors.join(", "));
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle("API Example")
+    .setDescription("API documentation")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
